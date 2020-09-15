c41fbbd6b7ff17f8d7c924ecd6cd2e872f0c0900
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-15 18:56:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,5 +16,9 @@ def loop_message_n_times(string, integer)
 end
 
 def output_array(array)
-  
+  counter = 0 
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end 
