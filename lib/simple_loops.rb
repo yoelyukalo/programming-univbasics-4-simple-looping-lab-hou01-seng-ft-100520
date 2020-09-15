3c8f5015ff7226971404b1d2f1bcfe098320e811
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-15 18:47:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,8 +6,9 @@ def loop_message_five_times(string)
   end
 end
 
-def loop_message_n_times(string, integer)
+def loop_message_n_times(message, limit)
   counter = integer
+  counter = 0
   while counter < integer do 
     puts string
     counter += 1 
