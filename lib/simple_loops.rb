409bc2c48988cace19c0fc88da188447804697c2
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-15 19:02:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,8 +26,10 @@ end
 
 def reurn_string_array(array)
   count = 0
-  while count < array.length do 
-    puts array[count].to_s
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
     count += 1 
   end
+  new_array
 end
